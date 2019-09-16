d673e044181db70df03deb41e1ed044b74cc2f4e
Felipe Parra <fpr29.10@gmail.com>
2019-09-16 13:07:58 -0500

    updated 2

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 mongoose
-	.connect(process.env.DB_URI, { useNewUrlParser: true, useCreateIndex: true })
-	.then((x) => {
-		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
-	})
-	.catch((err) => {
-		console.error('Error connecting to mongo', err);
-	});
+  .connect(process.env.DB_URI, { useNewUrlParser: true, useCreateIndex: true })
+  .then(x => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo ", err);
+  });
--- routes/auth.js
+++ routes/auth.js
@@ -1,65 +1,70 @@
-const router = require('express').Router();
-const User = require('../models/Users');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { registerValidation, loginValidation } = require('../middlewares/validations');
+const router = require("express").Router();
+const User = require("../models/Users");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const {
+  registerValidation,
+  loginValidation
+} = require("../middlewares/validations");
 
-router.get('/all', (req, res, next) => {
-	User.find()
-		.then((users) => {
-			res.json(users);
-		})
-		.catch((err) => {
-			res.status(400).json('Error: ' + err);
-		});
+router.get("/all", (req, res, next) => {
+  User.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(400).json("Error: " + err);
+    });
 });
 
-router.post('/register', async (req, res, next) => {
-	// validate the data before we a user
-	const { error } = registerValidation(req.body);
-	if (error) return res.status(400).json(error.details[0].message);
+router.post("/register", async (req, res, next) => {
+  // validate the data before we a user
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).json(error.details[0].message);
 
-	// Checking if the user is already in the database
-	const emailExist = await User.findOne({ email: req.body.email });
-	if (emailExist) return res.status(400).json('Email already exists');
+  // Checking if the user is already in the database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).json("Email already exists");
 
-	// HASH passwords
-	const salt = await bcrypt.genSalt(10);
-	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  // HASH passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-	try {
-		// Create a new user
-		User.create({ ...req.body, password: hashedPassword })
-			.then((user) => {
-				res.json(user._id);
-			})
-			.catch((err) => {});
-	} catch (error) {
-		res.status(400).json('Error: ' + error);
-	}
+  try {
+    // Create a new user
+    User.create({ ...req.body, password: hashedPassword })
+      .then(user => {
+        res.json(user._id);
+      })
+      .catch(err => {
+        res.status(400).json("Error: " + err);
+      });
+  } catch (error) {
+    res.status(400).json("Error: " + error);
+  }
 });
 
-router.post('/login', async (req, res, next) => {
-	// validate the data before we a user
-	const { error } = loginValidation(req.body);
-	if (error) return res.status(400).json(error.details[0].message);
+router.post("/login", async (req, res, next) => {
+  // validate the data before we a user
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).json(error.details[0].message);
 
-	// Checking if the user is exists
-	const user = await User.findOne({ email: req.body.email });
-	if (!user) return res.status(400).json('Email or password is incorrect');
+  // Checking if the user is exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).json("Email or password is incorrect");
 
-	// PASSWORD IS CORRECT
-	const validPass = await bcrypt.compare(req.body.password, user.password);
-	if (!validPass) return res.status(400).json('Email or Password is incorrect');
+  // PASSWORD IS CORRECT
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).json("Email or Password is incorrect");
 
-	// Create and assign a token
-	const token = jwt.sign({ __id: user.__id }, process.env.TOKEN_SECRET);
+  // Create and assign a token
+  const token = jwt.sign({ __id: user.__id }, process.env.TOKEN_SECRET);
 
-	try {
-		res.header('auth-token', token).json(token);
-	} catch (error) {
-		res.status(400).json('Error: ' + error);
-	}
+  try {
+    res.header("auth-token", token).json(token);
+  } catch (error) {
+    res.status(400).json("Error: " + error);
+  }
 });
 
 module.exports = router;
